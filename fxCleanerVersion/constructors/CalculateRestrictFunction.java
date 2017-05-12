c14d2710c72c5d0b31aba62a5658e41521b7bc83
stpati <Patricia@172.16.33.236>
2017-05-12 15:59:21 +0200

    24234

--- fxCleanerVersion/constructors/CalculateRestrictFunction.java
+++ fxCleanerVersion/constructors/CalculateRestrictFunction.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class CalculateRestrictFunction {
 
 	double factor1, factor2, border;
+	double wertbeiX, wertbeiY, ergebnis;
 	int resticts;
 	
 	
@@ -14,18 +15,36 @@ public class CalculateRestrictFunction {
 		factor1= Double.parseDouble(restrictList.get(4).toString());
 		factor2= Double.parseDouble(restrictList.get(5).toString());
 		border=Double.parseDouble(restrictList.get(6).toString());
+			
 		
 	}
 	
-	public void calculate()
+	public void calculate(List<Object> restrictList)
 	{
-		double a1=(-1)*factor1/factor2;
-		double a2=border/factor2;
-		factor1=a1;
-		factor2=a2;
+		
+		for(int i=4; i<= (restrictList.size()-1);i=i+3){
+			wertbeiX = Double.parseDouble(restrictList.get(i).toString());
+			wertbeiY = Double.parseDouble(restrictList.get(i++).toString());
+			ergebnis = Double.parseDouble(restrictList.get(i+2).toString());
+			
+	
+		
+			
+		}
+		
+		
+		
+		double a1=(-1)*wertbeiX/wertbeiY;
+		double a2=ergebnis/wertbeiY;
+		wertbeiX=a1;
+		wertbeiY=a2;
+		
 		
 		
 	}
+	
+	
+	
 
 	public double getFactor1() {
 		return factor1;
